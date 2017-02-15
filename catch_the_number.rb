4cf913b14e2966a4e1076d6a4a390e3a7c37b35c
Reni <rgabrielsoto@gmail.com>
2017-02-15 14:33:35 -0600

    Exercises fron Loop 1 & 2 exercises

---
+++ catch_the_number.rb
@@ -0,0 +1,9 @@
+loop do
+  number = rand(100)
+  puts number
+
+  if number.between?(0, 10)
+    break
+  end
+end
+
---
+++ loop_on_command.rb
@@ -0,0 +1,6 @@
+loop do
+  puts 'Should I stop looping?'
+  answer = gets.chomp.downcase
+  break if answer == 'yes'
+end
+
